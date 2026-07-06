b3a94215b854b1f497acad01214acb766f13954e
rajlaxmi12 <137147812+rajlaxmi12@users.noreply.github.com>
2026-07-06 20:06:25 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,9 +16,10 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin: 'http://localhost:5173',
-    credentials:true
-}
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials: true,
+};
+
 app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
